40ad23c74248eb30b04d2a3e693a7c01cc964eea
nxsyed <nxsyed.ahmed@gmail.com>
2019-04-01 12:51:26 -0700

    Added docker config

--- application/urls.py
+++ application/urls.py
@@ -26,7 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-  url(r'^admin/', include(admin.site.urls)),
+  url(r'^django-admin/', admin.site.urls),
   url(r'^', include(router.urls)),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
